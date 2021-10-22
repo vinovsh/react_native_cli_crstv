19b910240502fee0f0b142ec48a23cc0e48232b3
madhavan1098 <madhavan1098@gmail.com>
2021-10-22 12:06:48 +0530

    first commit

--- Screens/ViewImageScreen.js
+++ Screens/ViewImageScreen.js
@@ -1,14 +1,19 @@
 import React from 'react';
-import {View,Text,Button, StyleSheet,Dimensions,Animated, Image} from "react-native";
+import {View,Text,Button, StyleSheet,Dimensions,Animated, Image,TouchableOpacity} from "react-native";
 import ProgressiveImage from '../Components/ProgressiveImage';
 import { PinchGestureHandler,State } from 'react-native-gesture-handler';
+import Feather from 'react-native-vector-icons/Feather';
 
 const width=Dimensions.get("window").width;
 const height=Dimensions.get('window').height;
 const ViewImageScreen = ({route,navigation}) => {
 
  console.log(route.params)
- navigation.setOptions({ title:route.params.title });
+ 
+ React.useEffect(() => {
+  navigation.setOptions({ title:route.params.title });
+
+}, []);
  const scale=new Animated.Value(1)
  const onZoomEventFunction=Animated.event(
    
@@ -61,6 +66,20 @@ const ViewImageScreen = ({route,navigation}) => {
                 />
          </PinchGestureHandler>
             </View>
+           <View style={styles.download_box}>
+             
+              <View style={styles.download_circle}>
+                <TouchableOpacity>    
+                  <Feather 
+                    name="download"
+                    color="white"
+                    size={25}
+                  />
+                  </TouchableOpacity>
+              </View>
+             
+          </View> 
+           
           
         </View>
     );
@@ -72,11 +91,12 @@ const styles = StyleSheet.create({
 
     flex:1,
     
-    flexDirection:"row",
+    flexDirection:"column",
    justifyContent:"center",
    alignItems:"center",
   
-   backgroundColor:"black"
+   backgroundColor:"black",
+ 
    
     
     
@@ -88,5 +108,24 @@ const styles = StyleSheet.create({
     width:"100%",
     height:width,
   
+  },
+  download_box:{
+   /*  position:"relative",
+    bottom:30,
+    left:width/3,
+    justifyContent:"center",
+    alignItems:"center" */
+    marginTop:20,
+    width:"100%",
+   
+    
+
+  },
+  download_circle:{
+     justifyContent:"flex-end",
+     alignItems:"flex-end",
+     marginRight:"10%"
+     
   }
+
 })
